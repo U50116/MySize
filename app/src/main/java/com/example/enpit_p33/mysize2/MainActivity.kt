c1fdc8b204e355fe6883863627b5387b009151aa
u50116 <ap1817hy@apps.ube-k.ac.jp>
2018-09-05 13:06:00 +0900

    プッシュテスト

--- app/src/main/java/com/example/enpit_p33/mysize2/MainActivity.kt
+++ app/src/main/java/com/example/enpit_p33/mysize2/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
             waist.setText(editWaist)
             inseam.setText(editInseam)
         }
-
         save.setOnClickListener{onSaveTapped() }
 
         heightButton.setOnClickListener{
